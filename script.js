33221f59a11282977e7583fd06b88ace371852fc
chunarughatcity <chunarughatcity@gmail.com>
2024-10-29 23:44:19 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+function toggleMenu() {
+    const menu = document.getElementById('quickAccessMenu');
+    const hamburger = document.querySelector('.hamburger-menu');
+    menu.classList.toggle('active');
+    hamburger.classList.toggle('active');
+}
