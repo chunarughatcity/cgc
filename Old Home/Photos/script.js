c1b234aa4ce1ec10d63340e49aec81dfc940d34d
chunarughatcity <chunarughatcity@gmail.com>
2025-07-19 22:43:30 +0600

    Delete Old Home directory

--- Old Home/Photos/script.js
+++
@@ -1,6 +0,0 @@
-function toggleMenu() {
-    const menu = document.getElementById('quickAccessMenu');
-    const hamburger = document.querySelector('.hamburger-menu');
-    menu.classList.toggle('active');
-    hamburger.classList.toggle('active');
-}
